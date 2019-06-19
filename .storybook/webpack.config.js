e4b217a7f6d9459dbe3f7eafbc9a90844a6db6f7
GastroGeek <gastrogeek@gmail.com>
2019-06-19 19:51:25 -0400

    Add css extract plugin and bundle analyzer plugin via ANALYZE env variable

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,21 @@
 const { resolve } = require('path')
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = async ({ config, mode }) => {
   config.module.rules.push({
     test: /\.s?css$/,
     loaders: [
       {
         loader: 'style-loader'
+      },
+      {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          publicPath: '../',
+          hmr: process.env.NODE_ENV === 'development'
+        }
       },
       {
         loader: 'css-loader',
@@ -29,6 +39,20 @@ module.exports = async ({ config, mode }) => {
     include: resolve(__dirname, '../'),
   });
 
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[name].css'
+    })
+  )
+
+  if (process.env.ANALYZE) {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: true
+      })
+    )
+  }
 
   config.resolve.alias['@'] = resolve(__dirname, '../src')
   config.resolve.alias['@utils'] = resolve(__dirname, 'utils')
