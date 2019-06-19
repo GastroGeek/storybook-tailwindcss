b8cef32446c794e44f2489ed634dc85d9a48144f
GastroGeek <gastrogeek@gmail.com>
2019-06-19 19:49:53 -0400

    Add purgecss to reduce bundle size(s)

--- .storybook/.postcssrc.js
+++ .storybook/.postcssrc.js
@@ -1,8 +1,18 @@
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx'
+  ],
+
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.config.js'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
   ]
 }
